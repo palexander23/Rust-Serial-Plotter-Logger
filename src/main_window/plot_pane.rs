b2b0665c83ddda988f8a049c8ae6d1dc4e0d7d59
Pete Alexander <Pete.Alexander@42T.com>
2022-10-20 12:36:05 +0100

    Remove Plot window heading and group box
    Makes the plot window bigger and less cluttered

--- src/main_window/plot_pane.rs
+++ src/main_window/plot_pane.rs
@@ -19,28 +19,25 @@ impl PlotPane {
 
     pub fn update(&mut self, ui: &mut egui::Ui) {
         // Plot the values stored in the local line storage
-        ui.group(|ui| {
-            ui.heading("Serial Plot");
-            match self.lines.lock() {
-                Ok(lines) => {
-                    // Process the stored plot values into lines to be plotted
-                    let plot_lines: Vec<egui::plot::Line> = lines
-                        .line_vec
-                        .iter()
-                        .map(|lv| lv.get_plot_values())
-                        .map(|pv| egui::plot::Line::new(pv))
-                        .map(|l| l.width(2.0))
-                        .collect();
+        match self.lines.lock() {
+            Ok(lines) => {
+                // Process the stored plot values into lines to be plotted
+                let plot_lines: Vec<egui::plot::Line> = lines
+                    .line_vec
+                    .iter()
+                    .map(|lv| lv.get_plot_values())
+                    .map(|pv| egui::plot::Line::new(pv))
+                    .map(|l| l.width(2.0))
+                    .collect();
 
-                    // Generate the plot window
-                    Plot::new("my_plot")
-                        .show_axes([false, true])
-                        .show(ui, |plot_ui| {
-                            plot_lines.into_iter().for_each(|l| plot_ui.line(l));
-                        });
-                }
-                Err(e) => error!("Could not get lock on line data: {:?}", e),
-            };
-        });
+                // Generate the plot window
+                Plot::new("my_plot")
+                    .show_axes([false, true])
+                    .show(ui, |plot_ui| {
+                        plot_lines.into_iter().for_each(|l| plot_ui.line(l));
+                    });
+            }
+            Err(e) => error!("Could not get lock on line data: {:?}", e),
+        };
     }
 }

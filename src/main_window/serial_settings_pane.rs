489f23c14f67c053f040fdf733331ea8c1079b78
Pete Alexander <Pete.Alexander@42t.com>
2022-10-21 12:06:27 +0100

    Make baud,port comboboxes remember position
    Sort port names by number, not alphabetically

--- src/main_window/serial_settings_pane.rs
+++ src/main_window/serial_settings_pane.rs
@@ -3,36 +3,43 @@ use eframe::{
     emath::Align,
 };
 use serialport;
-use strum::{EnumIter, IntoEnumIterator};
+use strum::IntoEnumIterator;
 
-use crate::baud::{self, Baud};
+use crate::baud::Baud;
 
 pub struct SerialSettingsPane {
-    text1: String,
-    text2: String,
+    selected_port_name_idx: usize,
+    selected_baud_rate_idx: usize,
 }
 
 impl SerialSettingsPane {
     pub fn new() -> Self {
         Self {
-            text1: Default::default(),
-            text2: Default::default(),
+            selected_port_name_idx: Default::default(),
+            selected_baud_rate_idx: Default::default(),
         }
     }
 
     pub fn update(&mut self, ui: &mut egui::Ui) {
         // Get list of available serial port names
-        let available_port_names: Vec<String> = serialport::available_ports()
-            .unwrap()
-            .iter()
-            .map(|s| s.port_name.clone())
-            .collect();
+        let mut available_ports = serialport::available_ports().unwrap();
 
-        let mut selected_port_name_idx: usize = 0;
+        // Sort port list numerically
+        available_ports.sort_by_key(|p| {
+            p.port_name
+                .replace("COM", "")
+                .to_owned()
+                .parse::<i32>()
+                .unwrap()
+        });
+
+        let available_port_names: Vec<String> = available_ports
+            .iter()
+            .map(|s| s.port_name.to_owned())
+            .collect();
 
         // Get list of possible Baud Rates
         let baud_rates: Vec<_> = Baud::iter().collect();
-        let mut selected_baud_rate_idx = 0;
 
         ui.group(|ui| {
             ui.with_layout(
@@ -44,7 +51,7 @@ impl SerialSettingsPane {
                         .show(ui, |ui| {
                             egui::ComboBox::from_label("Serial Port").show_index(
                                 ui,
-                                &mut selected_port_name_idx,
+                                &mut self.selected_port_name_idx,
                                 available_port_names.len(),
                                 |i| available_port_names[i].to_owned(),
                             );
@@ -52,7 +59,7 @@ impl SerialSettingsPane {
 
                             egui::ComboBox::from_label("Baud Rate").show_index(
                                 ui,
-                                &mut selected_baud_rate_idx,
+                                &mut self.selected_baud_rate_idx,
                                 baud_rates.len(),
                                 |i| baud_rates[i].into(),
                             );
